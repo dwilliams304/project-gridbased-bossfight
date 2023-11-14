7b94e7f42863a258b158c22468edfddb59876dc9
dwilliams304 <d.williams0434@gmail.com>
2023-11-14 11:25:03 -0700

    Main menu, buttons, import leantween/tmp

---
+++ Assets/_Scripts/Button.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private bool _soundOnHover, _scaleOnHover, _highlightOnHover;
+
+    [SerializeField] private Vector3 _sizeToScaleTo;
+    private Vector3 originalScale = new Vector3(1f, 1f, 1f);
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(_scaleOnHover) transform.LeanScale(_sizeToScaleTo, 0.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(_scaleOnHover) transform.LeanScale(originalScale, 0.1f);
+    }
+
+}
